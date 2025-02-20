aae6dac4cc4582aa6624865ee89149c12989f973
Eric Klaus <ericklaus16@gmail.com>
2025-02-20 19:22:41 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -26,20 +26,30 @@ function gerarFaturaStr (fatura, pecas) {
     return pecas[apresentacao.id];
   }
 
+  function calcularCredito(apre){
+    let creditos = 0;
+    creditos += Math.max(apre.audiencia - 30, 0);
+    if (getPeca(apre).tipo === "comedia") 
+       creditos += Math.floor(apre.audiencia / 5);
+    return creditos;   
+  }
+
+  function formatarMoeda(valor){
+    return new Intl.NumberFormat("pt-BR",
+      { style: "currency", currency: "BRL",
+        minimumFractionDigits: 2 }).format(valor/100);
+  }
+
   let totalFatura = 0;
   let creditos = 0;
   let faturaStr = `Fatura ${fatura.cliente}\n`;
-  const formato = new Intl.NumberFormat("pt-BR",
-                        { style: "currency", currency: "BRL",
-                          minimumFractionDigits: 2 }).format;
+  const formato = formatarMoeda;
 
   for (let apre of fatura.apresentacoes) {
     // const peca = getPeca(apre);
     let total = calcularTotalApresentacao(apre);
     // créditos para próximas contratações
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(apre).tipo === "comedia") 
-       creditos += Math.floor(apre.audiencia / 5);
+    creditos += calcularCredito(apre);
 
     // mais uma linha da fatura
     faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
